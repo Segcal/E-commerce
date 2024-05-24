578cc83968a0580f5060d5f9367651a5adc33123
Segcal <seguncaleb3@gmail.com>
2024-05-24 02:23:17 +0100

    commit

--- src/pages/LandingPage/components/Hero.jsx
+++ src/pages/LandingPage/components/Hero.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <main className="flex items-center mt-16 md:flex-row flex-col gap-6 bg-gray-100">
+    <main className="flex  items-center mt-16 md:flex-row flex-col gap-6 bg-gray-100">
       <div className="text-center md:text-start md:justify-start justify-center md:items-start items-center flex flex-col">
-        <h1 className="capitalize text-3xl lg:text-6xl">
+        <h1 className="capitalize text-3xl lg:text-6xl text-gray-600">
           discover <span className="text-yellow leading-10">comfort</span>{" "}
           <br className="hidden md:block" /> and{" "}
           <span className="text-yellow">style</span>
         </h1>
-        <p className="w-[20rem] py-4 text-[1.1rem] leading-7">
+        <p className="w-[20rem] py-4 text-[1.1rem] leading-7 text-gray-600">
           Explore our exclusive collection of chairs designed for every room and
           occasion.
         </p>
@@ -18,9 +18,9 @@ export default function Hero() {
         </button>
       </div>
       <div>
-        <main className='relative md:hidden'>
+        <main className="relative md:hidden">
           <div className="bg-purple w-72 h-72 rounded-full"></div>
-          <div className='absolute top-0'>
+          <div className="absolute top-0">
             <img
               src="https://res.cloudinary.com/dtrqikle5/image/upload/v1716243343/image_269_1_h4paf4.png"
               alt=""
--- src/pages/LandingPage/components/Tags.jsx
+++ src/pages/LandingPage/components/Tags.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 export default function Tags() {
   return (
     <>
-      <div className="flex items-center justify-center capitalize md:gap-32 gap-10 bg-white lg:w-1/2 md:w-[80%] w-[85%]  mx-auto py-8 rounded-xl md:-mt-12 -mt-8 shadow-lg">
-        <ul className="flex items-center flex-col">
+      <div className="text-gray-600 flex items-center justify-center capitalize md:gap-32 gap-10 bg-white lg:w-1/2 md:w-[80%] w-[95%]  mx-auto py-8 rounded-xl md:-mt-12 -mt-4 shadow-lg">
+        <ul className="flex items-center flex-col ">
           <h2 className="text-xl font-semibold">1K</h2>
-          <p className="text-[0.5rem]">brands</p>
+          <p className="sm:text-sm text-[0.7rem]">brands</p>
         </ul>
         <ul className="flex items-center flex-col border-l md:px-24 px-10 border-r">
           <h2 className="text-xl font-semibold">500K+</h2>
-          <p className="text-[0.5rem]">customers</p>
+          <p className="sm:text-sm text-[0.7rem]">customers</p>
         </ul>
         <ul className="flex items-center flex-col">
           <h2 className="text-xl font-semibold">200K+</h2>
-          <p className="text-[0.5rem]">Active merchants</p>
+          <p className="sm:text-sm text-[0.7rem]">Active merchants</p>
         </ul>
       </div>
     </>
