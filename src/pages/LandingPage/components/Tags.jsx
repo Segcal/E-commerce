bcbd045a133d03b206e93f4f91fabe91b1f8ed3a
Segcal <seguncaleb3@gmail.com>
2024-05-24 02:34:04 +0100

    commit

--- src/pages/LandingPage/components/Tags.jsx
+++ src/pages/LandingPage/components/Tags.jsx
@@ -3,19 +3,21 @@ import React from 'react'
 export default function Tags() {
   return (
     <>
-      <div className="text-gray-600 flex items-center justify-center capitalize md:gap-32 gap-10 bg-white lg:w-1/2 md:w-[80%] w-[95%]  mx-auto py-8 rounded-xl md:-mt-12 -mt-4 shadow-lg">
-        <ul className="flex items-center flex-col ">
-          <h2 className="text-xl font-semibold">1K</h2>
-          <p className="sm:text-sm text-[0.7rem]">brands</p>
-        </ul>
-        <ul className="flex items-center flex-col border-l md:px-24 px-10 border-r">
-          <h2 className="text-xl font-semibold">500K+</h2>
-          <p className="sm:text-sm text-[0.7rem]">customers</p>
-        </ul>
-        <ul className="flex items-center flex-col">
-          <h2 className="text-xl font-semibold">200K+</h2>
-          <p className="sm:text-sm text-[0.7rem]">Active merchants</p>
-        </ul>
+      <div className="text-gray-600 flex items-center lg:w-1/2 md:w-[85%] justify-center capitalize md:gap-32 gap-10 bg-white   mx-auto py-8 rounded-xl md:-mt-12 -mt-4 shadow-lg">
+        <main className='flex items-center gap-6'>
+          <ul className="flex items-center flex-col ">
+            <h2 className="text-xl font-semibold">1K</h2>
+            <p className="sm:text-sm text-[0.7rem]">brands</p>
+          </ul>
+          <ul className="flex items-center flex-col border-l md:px-24 px-10 border-r">
+            <h2 className="text-xl font-semibold">500K+</h2>
+            <p className="sm:text-sm text-[0.7rem]">customers</p>
+          </ul>
+          <ul className="flex items-center flex-col">
+            <h2 className="text-xl font-semibold">200K+</h2>
+            <p className="sm:text-sm text-[0.7rem]">Active merchants</p>
+          </ul>
+        </main>
       </div>
     </>
   );
